627ed9f01a0ea467837f5061ac7c4dd2681644f6
Rafael Ernesto <scbllu.200@gmail.com>
2024-09-11 10:28:43 -0600

    commit

--- components/Navegacion.jsx
+++ components/Navegacion.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { Link } from "expo-router";
 import { LogIn, Menu, Tag, Info, Users } from "lucide-react-native";
 
