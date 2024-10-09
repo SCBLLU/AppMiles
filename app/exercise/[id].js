363fc57fd9ad4d258fd792ac5f595dfdf6d7d8ea
SCBLLU <scbllu.200@gmail.com>
2024-10-08 21:28:21 -0600

    Actualizar estilos de encabezado en User y agregar funcionalidad de zoom en la imagen

--- app/exercise/[id].js
+++ app/exercise/[id].js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity, ScrollView } from "react-native";
 import { useLocalSearchParams, Stack } from "expo-router";
 import data from "../../data/data.json";
 import { useDarkMode } from "../../components/Utils/DarkModeProvider";
@@ -44,14 +44,35 @@ const ExerciseDetails = () => {
           ),
         }}
       />
-      <View style={[styles.container, { backgroundColor: isDarkMode ? "#121212" : "#fff" }]}>
-        <Text style={[styles.title, { color: isDarkMode ? "#fff" : "#000" }]}>{exercise.name}</Text>
-        <Text style={[styles.description, { color: isDarkMode ? "#b3b3b3" : "#666" }]}>
-          Duración: {exercise.duration} minutos{"\n"}
-          Calorías quemadas: {exercise.calories}{"\n"}
-          Categoría: {category ? category.name : "Desconocida"}
-        </Text>
-      </View>
+
+      <ScrollView style={{ backgroundColor: isDarkMode ? "#121212" : "#fff" }}>
+
+        <View style={[styles.container, { backgroundColor: isDarkMode ? "#121212" : "#fff" }]}>
+          <Image source={{ uri: exercise.image }} style={styles.image} />
+          <Text style={[styles.title, { color: isDarkMode ? "#fff" : "#000" }]}>{exercise.name}</Text>
+          <Text style={[styles.description, { color: isDarkMode ? "#b3b3b3" : "#666" }]}>
+            Duración: {exercise.duration} minutos{"\n"}
+            Calorías quemadas: {exercise.calories}{"\n"}
+            Categoría: {category ? category.name : "Desconocida"}
+          </Text>
+
+          <View style={styles.extraInfo}>
+            <Text style={[styles.extraTitle, { color: isDarkMode ? "#fff" : "#000" }]}>Músculos Trabajados:</Text>
+            <Text style={[styles.extraText, { color: isDarkMode ? "#b3b3b3" : "#666" }]}>
+              {exercise.muscles.join(", ")}
+            </Text>
+            <Text style={[styles.extraTitle, { color: isDarkMode ? "#fff" : "#000" }]}>Consejos:</Text>
+            <Text style={[styles.extraText, { color: isDarkMode ? "#b3b3b3" : "#666" }]}>
+              {exercise.tips}
+            </Text>
+          </View>
+
+          <TouchableOpacity style={styles.startButton} onPress={() => alert("¡Ejercicio iniciado!")}>
+            <Text style={styles.startButtonText}>Realizar Ejercicio</Text>
+          </TouchableOpacity>
+        </View>
+
+      </ScrollView>
     </>
   );
 };
@@ -72,13 +93,41 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   categoryContainer: {
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    borderRadius: 8,
+    padding: 10,
+    borderRadius: 10,
   },
   categoryText: {
-    fontSize: 14,
     color: "#fff",
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    borderRadius: 10,
+    marginBottom: 20,
+  },
+  extraInfo: {
+    marginVertical: 10,
+  },
+  extraTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  extraText: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  startButton: {
+    backgroundColor: "#282c34",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+    marginTop: 20,
+  },
+  startButtonText: {
+    color: "#fff",
+    fontSize: 18,
     fontWeight: "bold",
   },
 });
